23e3f01be98be2b0fe7ee2b0d35fe6fdbd9a553a
Jens Wurm <jens.wurm@umit.at>
2024-10-09 01:56:03 +0200

    Add normal form for gantry, add feed forward with filter for gantry

--- brueckenkran/feedforward.py
+++ brueckenkran/feedforward.py
@@ -7,6 +7,20 @@ def feedForwardRamp(t0, T, yd):
     uIn = lambda t: yd if t0 < t < t0 + T else 0
     return uIn
 
+def feedForwardFilter(t0, yd):
+    a0 = 2 ** 5
+    yr = np.array([
+            lambda t: a0 * yd*(-2*t**4*np.exp(2*t0) + 8*t**3*t0*np.exp(2*t0) - 4*t**3*np.exp(2*t0) - 12*t**2*t0**2*np.exp(2*t0) + 12*t**2*t0*np.exp(2*t0) - 6*t**2*np.exp(2*t0) + 8*t*t0**3*np.exp(2*t0) - 12*t*t0**2*np.exp(2*t0) + 12*t*t0*np.exp(2*t0) - 6*t*np.exp(2*t0) - 2*t0**4*np.exp(2*t0) + 4*t0**3*np.exp(2*t0) - 6*t0**2*np.exp(2*t0) + 6*t0*np.exp(2*t0) + 3*np.exp(2*t) - 3*np.exp(2*t0))*np.exp(-2*t)/96 if t >= t0 else 0,
+            lambda t: a0 * yd*(t**4 - 4*t**3*t0 + 6*t**2*t0**2 - 4*t*t0**3 + t0**4)*np.exp(-2*t + 2*t0)/24 if t >= t0 else 0,
+            lambda t: a0 * yd*(-t**4 + 4*t**3*t0 + 2*t**3 - 6*t**2*t0**2 - 6*t**2*t0 + 4*t*t0**3 + 6*t*t0**2 - t0**4 - 2*t0**3)*np.exp(-2*t + 2*t0)/12 if t >= t0 else 0,
+            lambda t: a0 * yd*(t**4 - 4*t**3*t0 - 4*t**3 + 6*t**2*t0**2 + 12*t**2*t0 + 3*t**2 - 4*t*t0**3 - 12*t*t0**2 - 6*t*t0 + t0**4 + 4*t0**3 + 3*t0**2)*np.exp(-2*t + 2*t0)/6 if t >= t0 else 0,
+            lambda t: a0 * yd*(-t**4 + 4*t**3*t0 + 6*t**3 - 6*t**2*t0**2 - 18*t**2*t0 - 9*t**2 + 4*t*t0**3 + 18*t*t0**2 + 18*t*t0 + 3*t - t0**4 - 6*t0**3 - 9*t0**2 - 3*t0)*np.exp(-2*t + 2*t0)/3 if t >= t0 else 0,
+        ])
+
+    uIn = lambda t: st.M * st.l / st.g * yr[4](t) + (st.M + st.m) * yr[2](t)
+    
+    return yr, uIn
+
 def feedForwardLinFlat(t0, T, yd):
     yr = np.array([
         lambda t: yd * (70 * (t - t0) ** 9 / T ** 9 - 315 * (t - t0) ** 8 / T ** 8 + 540 * (t - t0) ** 7 / T ** 7 - 420 * (t - t0) ** 6 / T ** 6 + 126 * (t - t0) ** 5 / T ** 5) if t0 <= t <= t0 + T else 0 if t < t0 else yd,
